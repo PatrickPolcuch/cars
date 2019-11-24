62b72341eb13d05502b73c14b946f3a7be556f0b
Patrick Polcuch <polcuch@chapman.edu>
2019-11-23 20:30:19 -0800

    cars.sh

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 This repo contains a bash script named “cars.sh” that will help a user maintain an inventory of old cars.
  
  The user will be presented with a menu of three options:
+1. Enter a new car
+..* Prompts user for the make model and type and adds it to a text file
+2. Displays a sorted list of the cars
+3. Quit and exit the program
---
+++ cars.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+#this is called cars.sh
+#By Patrick Polcuch
+while true
+do
+	echo "Type the number 1 to enter a new car \nType the number 2 to display the list of cars \n Type the number 3 to quit and exit the program"
+	read num
+	if [ $num == 1 ]
+	then
+		echo "Give me the Year then the Make then the Model"
+		read Year
+		read Make
+		read Model
+		echo $car >> My_old_cars.sh
+	fi
+	if [ $num == 2 ]
+	then
+		sort My_old_cars
+	fi
+	if [ $num == 3 ]
+	then
+		echo "Goodbye"
+		break
+	fi
+done
